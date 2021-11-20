dac796b1fdad884fce76e645ce560478cdb79f2d
gayuganeshan <94740074+gayuganeshan@users.noreply.github.com>
2021-11-20 15:14:05 +0530

    Create b.css

---
+++ b.css
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>MY CREATION</title>
+<body text="blue">
+<h1><u><center>GOVERNMENT OF ARTS AND SCIENCE COLLEGE</center></u></h2>
+<h2><u><center>VALPARAI-642 127</center></u></h2><br>
+<h2><u><center>COURSES OFFERED</center></u></h2>
+<h2><u><UG COURSES</u></h2><h2><u><center>PG COURSES</center></u></h2>
+<h2>            <a               href="detail.html">Computer         science          </a></h2><h2><center>           <ahref="Detail.html">Computer science</center></a></h2>
+<h2>            <a                                href="com.html">Commerce</a></h2><h2><center><a href="com.html">Commerce</center></a></h2>
+</head>
+</html>
